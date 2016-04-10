0093b49d281c9acf68cd4125358139424a65e7ba
Wei Xian Low <weix.low@gmail.com>
2016-04-10 17:59:17 -0500

    managed to show files, but slider doesn't work

--- filecheck.php
+++ filecheck.php
@@ -1,19 +1,33 @@
 <?php
 
-    $count = 1;
-    
-    echo '<div class="carousel-inner" role="listbox">';
-    
-    foreach (glob("*.jpg") as $filename) {
-        if(count == 1){
-            echo '<div class="item active"><img src="'.$filename.'" alt="'$filename'" width="460" height="345"></div>';
-            count++;
-        }else
-        {
-            echo '<div class="item"><img src="'.$filename.'" alt="'$filename'" width="460" height="345"></div>';
-            count++;
+    //The directory (relative to this file) that holds the images
+    $dir = "Images";
+
+
+    //This array will hold all the image addresses
+    $result = array();
+
+    //Get all the files in the specified directory
+    $files = scandir($dir);
+
+
+    foreach($files as $file) {
+
+        switch(ltrim(strstr($file, '.'), '.')) {
+
+            //If the file is an image, add it to the array
+            case "JPG": case "jpeg":case "png":case "gif":case "jpg":
+
+                $result[] = $dir . "/" . $file;
+
         }
     }
-    
-    echo '</div>';
+
+    //Convert the array into JSON
+    $resultJson = json_encode($result);
+
+    //Output the JSON object
+    //This is what the AJAX request will see
+    echo($resultJson);
+
 ?>
