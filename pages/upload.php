bfd9129c47df648ec8b22939d7aa9bd149f3b441
Wei Xian Low <weix.low@gmail.com>
2016-04-27 12:31:20 -0500

    added .gif file type to allow acceptance of .gif file type when uploading

--- pages/upload.php
+++ pages/upload.php
@@ -1,11 +1,11 @@
 <?php
 if(isset($_FILES["file"]["type"]))
 {
-    $validextensions = array("jpeg", "jpg", "png");
+    $validextensions = array("jpeg", "jpg", "png", "gif");
     $temporary = explode(".", $_FILES["file"]["name"]);
     $file_extension = end($temporary);
     if ((($_FILES["file"]["type"] == "image/png") || ($_FILES["file"]["type"] == "image/jpg") || ($_FILES["file"]["type"] == "image/jpeg")
-    ) && ($_FILES["file"]["size"] < 1000000)//Approx. 100kb files can be uploaded.
+    ) || ($_FILES["file"]["type"] == "image/gif") && ($_FILES["file"]["size"] < 9000000)//Approx. 100kb files can be uploaded.
     && in_array($file_extension, $validextensions)) {
         if ($_FILES["file"]["error"] > 0)
         {
@@ -14,29 +14,29 @@ if(isset($_FILES["file"]["type"]))
         else
         {
             if (file_exists("./Images/" . $_FILES["file"]["name"])) {
-            echo "<div class='alert alert-info'>
-            <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
-        <strong>Info!</strong> The file you chose to upload may have already been uploaded
-        </div> ";
+                echo "<div class='alert alert-info'>
+                <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                <strong>Info!</strong> The file you chose to upload may have already been uploaded
+                </div> ";
                 echo "<p></p>";
                 echo "<p>Try renaming your and uploading it again. ";
             }else{
-        $sourcePath = $_FILES['file']['tmp_name']; // Storing source path of the file in a variable
-        $targetPath = "./Images/".$_FILES['file']['name']; // Target path where file is to be stored
-                move_uploaded_file($sourcePath,$targetPath) ; // Moving Uploaded file
-        echo "<div class='alert alert-success'>
-  <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
-  <strong>Success!</strong> Your picture has been uploaded! 
-</div><br/>";
-        echo "<br/><b>File Name:</b> " . $_FILES["file"]["name"] . "<br>";
-        echo "<b>Type:</b> " . $_FILES["file"]["type"] . "<br>";
-        echo "<b>Size:</b> " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-        echo "<b>Temp file:</b> " . $_FILES["file"]["tmp_name"] . "<br>";
-        echo "<p></p>";
-        echo "<p></p>";
-        echo "<p>Your picture has been uploaded! Head back to the home page and take a look! </p>";
-                
-        }
+                $sourcePath = $_FILES['file']['tmp_name']; // Storing source path of the file in a variable
+                $targetPath = "./Images/".$_FILES['file']['name']; // Target path where file is to be stored
+                        move_uploaded_file($sourcePath,$targetPath) ; // Moving Uploaded file
+                echo "<div class='alert alert-success'>
+          <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+          <strong>Success!</strong> Your picture has been uploaded! 
+        </div><br/>";
+                echo "<br/><b>File Name:</b> " . $_FILES["file"]["name"] . "<br>";
+                echo "<b>Type:</b> " . $_FILES["file"]["type"] . "<br>";
+                echo "<b>Size:</b> " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+                echo "<b>Temp file:</b> " . $_FILES["file"]["tmp_name"] . "<br>";
+                echo "<p></p>";
+                echo "<p></p>";
+                echo "<p>Your picture has been uploaded! Head back to the <a href='http://wow.centralus.cloudapp.azure.com/finalproject'>home page</a> and take a look! </p>";
+
+            }
         }
         }
         else
--- pages/upload_js.js
+++ pages/upload_js.js
@@ -24,11 +24,11 @@ $("#file").change(function() {
 $("#message").empty(); // To remove the previous error message
 var file = this.files[0];
 var imagefile = file.type;
-var match= ["image/jpeg","image/png","image/jpg"];
-if(!((imagefile==match[0]) || (imagefile==match[1]) || (imagefile==match[2])))
+var match= ["image/jpeg","image/png","image/jpg", "image/gif"];
+if(!((imagefile==match[0]) || (imagefile==match[1]) || (imagefile==match[2]) || (imagefile==match[3])))
 {
 $('#previewing').attr('src','noimage.png');
-$("#message").html("<p id='error'>Please Select A valid Image File</p>"+"<h4>Note</h4>"+"<span id='error_message'>Only jpeg, jpg and png Images type allowed</span>");
+$("#message").html("<p id='error'>Please Select A valid Image File</p>"+"<h4>Note</h4>"+"<span id='error_message'>Only jpeg, jpg, gif and png Images type allowed</span>");
 return false;
 }
 else
