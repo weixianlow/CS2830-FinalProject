bd9af628dec4feae3e7d2a03bdae1ad1305e4e41
Wei Xian Low <weix.low@gmail.com>
2016-04-27 22:59:13 -0500

    edited the return message to include less technical information

--- pages/upload.php
+++ pages/upload.php
@@ -5,7 +5,7 @@ if(isset($_FILES["file"]["type"]))
     $temporary = explode(".", $_FILES["file"]["name"]);
     $file_extension = end($temporary);
     if ((($_FILES["file"]["type"] == "image/png") || ($_FILES["file"]["type"] == "image/jpg") || ($_FILES["file"]["type"] == "image/jpeg")
-    ) || ($_FILES["file"]["type"] == "image/gif") && ($_FILES["file"]["size"] < 9000000)//Approx. 100kb files can be uploaded.
+    ) || ($_FILES["file"]["type"] == "image/gif") && ($_FILES["file"]["size"] < 9000000)
     && in_array($file_extension, $validextensions)) {
         if ($_FILES["file"]["error"] > 0)
         {
@@ -21,9 +21,9 @@ if(isset($_FILES["file"]["type"]))
                 echo "<p></p>";
                 echo "<p>Try renaming your and uploading it again. ";
             }else{
-                $sourcePath = $_FILES['file']['tmp_name']; // Storing source path of the file in a variable
-                $targetPath = "./Images/".$_FILES['file']['name']; // Target path where file is to be stored
-                        move_uploaded_file($sourcePath,$targetPath) ; // Moving Uploaded file
+                $sourcePath = $_FILES['file']['tmp_name'];
+                $targetPath = "./Images/".$_FILES['file']['name']; 
+                move_uploaded_file($sourcePath,$targetPath) ;
                 echo "<div class='alert alert-success'>
           <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
           <strong>Success!</strong> Your picture has been uploaded! 
@@ -31,7 +31,6 @@ if(isset($_FILES["file"]["type"]))
                 echo "<br/><b>File Name:</b> " . $_FILES["file"]["name"] . "<br>";
                 echo "<b>Type:</b> " . $_FILES["file"]["type"] . "<br>";
                 echo "<b>Size:</b> " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-                echo "<b>Temp file:</b> " . $_FILES["file"]["tmp_name"] . "<br>";
                 echo "<p></p>";
                 echo "<p></p>";
                 echo "<p>Your picture has been uploaded! Head back to the <a href='http://wow.centralus.cloudapp.azure.com/finalproject'>home page</a> and take a look! </p>";
